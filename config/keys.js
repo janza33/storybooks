d5fd2d2510cb5f12e04bae0e03291695e3ded298
Jan <zajfertjan@gmail.com>
2019-07-15 12:29:49 +0200

    started UI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:"mongodb://MadPad:VoDaFone1!@ds249967.mlab.com:49967/storybooks-dev1",
+  mongoURI:"mongodb://janzajfert:-1killer1-@ds249967.mlab.com:49967/storybooks-dev1",
   googleClientID:"1025656171753-k5jv8i7rpubfren1fr5lq6bhe0avum57.apps.googleusercontent.com",
   googleClientSecret:"e0f2C_W2mAIpIsW_uDl38BWu"
 };
